c898ebcf50b87c8df08c5336c68df837f84ab2a7
lucasneves20 <heraldofdevs.com>
2024-03-27 08:21:31 -0300

    reta final do módulo

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -30,7 +30,8 @@ export const Disabled: StoryObj<TextInputProps> = {
 
 export const WithPrefix: StoryObj<TextInputProps> = {
   args: {
-    prefix: "cal.com/"
+    prefix: "cal.com/",
+    placeholder: 'your-username'
   }
 }
 
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -7,6 +7,16 @@ import { TextInput, TextInputProps } from './components/TextInput'
 import { TextArea, TextAreaProps } from './components/TextArea'
 import { Checkbox, CheckboxProps } from './components/Checkbox'
 import { MultiStep, MultiStepProps } from './components/MultiStep'
+import {
+  config,
+  css,
+  createTheme,
+  getCssText,
+  globalCss,
+  keyframes,
+  styled,
+  theme,
+} from './styles'
 
 export {
   Box,
@@ -18,6 +28,14 @@ export {
   TextArea,
   Checkbox,
   MultiStep,
+  config,
+  css,
+  createTheme,
+  getCssText,
+  globalCss,
+  keyframes,
+  styled,
+  theme,
 }
 
 export type {
